2885c95b8feb8aedcb384b8c5d13f99dd95bc687
jhne <elgenstierna@gmail.com>
2015-06-22 23:46:22 +0200

    Various fixes + new nav

--- inc/main.js
+++ inc/main.js
@@ -1,29 +1,48 @@
 $(document).ready(function() {
-	$(".up").click(function() {
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-		return false;
-	});
+    $(".up").click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+        return false;
+    });
 
-	$('.show').click(function () {
-		$('nav li a').not('.k-nav-current').toggle();
-		$('.show').toggleClass('show-toggled');
-		return false
-	});
+    $('.show').click(function() {
+        $('nav li a').not('.k-nav-current').toggle();
+        $('.show').toggleClass('show-toggled');
+        return false
+    });
 
-	var albumCount = $('.amount-check').html();
-	if (albumCount === '1') {
-		$('#slideshow_nav').hide();
-	};
+    var albumCount = $('.amount-check').html();
+    if (albumCount === '1') {
+        $('#slideshow_nav').hide();
+    };
 
-	function responsiveNav () {
-        if ($(window).width() <= 540){  
+    function responsiveNav() {
+        if ($(window).width() <= 540) {
             $('nav').addClass('mobile');
             $('.show').addClass('mobileOpen');
-        }   
+        }
     }
     responsiveNav();
 
-    $('.mobileOpen').click(function () {
-    	$('nav.mobile ul').toggle();
+    $('.mobileOpen').click(function() {
+        $('nav.mobile ul').toggle();
     });
+
+    $("#hover").mouseenter(function() {
+        var main = $('main');
+        var area = $('#hover');
+        var name = $('#sitename')
+        if (!main.hasClass('offset')) {
+            main.addClass('offset');
+            area.addClass('offset');
+            name.addClass('nameoffset');
+        }
+    });
+
+    $("main").mouseenter(function() {
+		$('main').removeClass('offset');
+		$('#hover').removeClass('offset');
+		$('#sitename').removeClass('nameoffset');
+	});
 });
